c455172967f1237c6b62fb4f1a2cb672c4e5f6a2
Dave Vasilevsky <dave@vasilevsky.ca>
2025-04-02 19:40:54 -0400

    login

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from monarch-fbar!")
+import asyncio
+import json
+
+from gql.transport.exceptions import TransportServerError
+from monarchmoney import MonarchMoney, LoginFailedException
+
+
+async def redo_login() -> MonarchMoney:
+    mm = MonarchMoney()
+    await mm.interactive_login(use_saved_session=False)
+    return mm
+
+
+async def login() -> MonarchMoney:
+    mm = MonarchMoney()
+    try:
+        await mm.login()
+        await mm.get_accounts()  # test that we're authenticated
+        return mm
+    except LoginFailedException:
+        return await redo_login()
+    except TransportServerError as e:
+        if e.code != 401:
+            raise e
+        return await redo_login()
+
+
+async def main():
+    mm = await login()
+    accounts = await mm.get_accounts()
+    print(json.dumps(accounts, indent=2))
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
